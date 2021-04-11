bdf6a52645ccb38aef70dc09056678780d209757
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-04-11 18:19:40 +0530

    boiler Code with StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {  // Build method gets called whenever we create a new version of this widget
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        body: SafeArea(
+          child: Container(  // Container can have only single child
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text("hello"),
+          ),
+        ),
+      ),
+    );
+  }
 }
