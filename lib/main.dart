2030130311811ec3a8b849a1431b4aa25dc434f6
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-04-14 12:17:10 +0530

    Mi_Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,18 +12,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {  // Build method gets called whenever we create a new version of this widget
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(  // Container can have only single child
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Text("hello"),
-          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+
+                backgroundImage: AssetImage('images/s.jpg'),
+              ),
+              Text(
+                'Sourodeep Ghosh Roy',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'
+
+                ),
+              ),
+
+              Text(
+                'FLUTTER DEVELOPER',
+                 style:TextStyle(
+                   fontSize: 20.0,
+                   letterSpacing: 2.5,
+                   color:Colors.teal[100],
+                   fontWeight: FontWeight.bold
+                 )
+              ),
+              SizedBox(
+                  height: 40.0,
+                  width: 150.0,
+                  child:Divider(
+                    color: Colors.teal[100],
+                  )
+              ),
+              Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child:ListTile(
+                  leading:Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title:Text(
+                    '+91 9871597556',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color:Colors.teal[800]
+                    ),
+                  )
+              )
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child:ListTile(
+                    leading:Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title:Text(
+                      'ghoshroy23@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color:Colors.teal[800]
+                      ),
+                    )
+                  )
+              ),
+              SizedBox(
+                height: 100.0,
+              )
+            ],
+          )
         ),
       ),
     );
   }
 }
+
+
